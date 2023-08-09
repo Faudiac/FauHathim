685b6990a16752f660280af56eb2ff1bceb3012e
Faudiac <115066002+Faudiac@users.noreply.github.com>
2023-08-09 19:38:51 +0500

    yes

---
+++ Mobile/js/search.js
@@ -0,0 +1,46 @@
+function openPage() {
+    var x = document.getElementById("mySearch").value;
+
+    if (x === "Home") {
+        window.open("../index.html");
+    }
+
+    if (x === "Dua's") {
+      window.open("../Dua's/Dua.html");
+    }
+
+    if (x === "Morning and Evening dua") {
+      window.open("../Dua's/Morning&Evening.html");
+    }
+
+    if (x === "Al-Yaseen") {
+        window.open("../Qur'an/Surah36.html");
+    }
+
+    if (x === "Al-Mulk") {
+      window.open("../Qur'an/Surah67.html");
+    }
+
+    if (x === "Al-Ikhlas") {
+        window.open("../Qur'an/Surah112.html");
+    }
+
+    if (x === "Al-Falaq") {
+      window.open("../Qur'an/Surah113.html");
+    }
+
+    if (x === "Al-Nas") {
+      window.open("../Qur'an/Surah114.html");
+    }
+
+    if (x === "Faudiax") {
+        window.open("https://faudiac.github.io/Faudiax/");
+    }
+
+    if (x === "FaudyNews") {
+      window.open("https://faudiac.github.io/OfficialFaudyNEWS/");
+    } 
+
+}
+
+  
